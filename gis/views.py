f266b9c891ef749f6d1ecfbcf0089e6a46e54f97
Vsevolod Demidov <fondad@ukr.net>
2020-03-23 12:29:11 +0200

    Urls was created. Url, Views and Temlates Home were created. Static was added

--- gis/views.py
+++ gis/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 
 
 # Create your views here.
-'''
 def home(request):
-    return HttpResponse("Hello world")
-'''
+    # return HttpResponse("Hello world")
+    return render(request, 'gis/home.html')
